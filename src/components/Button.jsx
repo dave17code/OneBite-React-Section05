cbca559ee7a35a6c7a56a52e5b8755199246fc7c
dave17code <dave17code@gmail.com>
2024-10-01 18:00:26 +0900

    EventHandler

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
 function Button({ text, color = "black", children }) {
   console.log(children);
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
