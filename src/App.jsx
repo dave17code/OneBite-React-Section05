b647fff6db16dee6b83d263b99b20778c2cc430c
dave17code <dave17code@gmail.com>
2024-10-03 07:14:08 +0900

    StateAndProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Button from "./components/Button";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  // const buttonProps = {
+  //   text: "메일",
+  //   color: "red",
+  //   a: 1,
+  //   b: 2,
+  //   c: 3,
+  // };
 
   return (
     <>
       {/* <Header />
       <Main />
       <Footer /> */}
-      <Button {...buttonProps} />
+      {/* <Button {...buttonProps} />
       <Button text={"카페"} />
       <Button text={"블로그"}>
         <Header />
-      </Button>
+      </Button> */}
+      <Bulb />
+      <Counter />
     </>
   );
 }
